b99c5a11c7a11f469e7e4d93db43cec88798c110
一叶之秋 <34859156+VJaGG@users.noreply.github.com>
2021-03-30 14:15:33 +0800

    Add files via upload

---
+++ DATA STRUCTURE/448. 找到所有数组中消失的数字.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+    vector<int> ans;
+    for (auto num: nums){
+        int pos = abs(num) - 1;
+        if (nums[pos] > 0){
+            nums[pos] = -nums[pos];
+        }
+    }
+    for (int i = 1; i <= nums.size(); i++){
+        if (nums[i-1] > 0){
+            ans.push_back(i);
+        }
+    }
+    return ans;
+}
+// [4,3,2,7,8,2,3,1]
+// [5,6]
+int main(){
+    vector<int> nums = {4, 3, 2, 7, 8, 2, 3, 1};
+    vector<int> ans = findDisappearedNumbers(nums);
+    for (auto i: ans){
+        printf("%d ", i);
+    }
+    return 0;
+}
